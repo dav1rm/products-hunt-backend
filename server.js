57adb57c52e42953225820ba2e82714b3364cdeb
Davi <davirodrigues8@gmail.com>
2019-02-20 22:46:35 -0300

    Adicionando permissao de acesso a api

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // Iniciando o app
 const app = express();
+// Permitir acesso a qualquer dominio
+app.use(cors());
 // Permitir o envio de dados com json
 app.use(express.json());
 
